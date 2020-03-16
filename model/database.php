7b3b942907c976ef0153e9340857a3a660e10371
Jordan Parsons <49204159+jordanparsons9925@users.noreply.github.com>
2020-03-16 11:37:59 -0230

    first work on database link

---
+++ model/database.php
@@ -0,0 +1,11 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=carebase';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        exit();
+    }
